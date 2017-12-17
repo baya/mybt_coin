a3824cf46b6c4f9dbbc746ccd6b8a82853e2cc75
baya <kayak.jiang@gmail.com>
2017-12-17 17:33:57 +0800

    working with ubuntu

--- src/kyk_utils.h
+++ src/kyk_utils.h
@@ -1,6 +1,8 @@
 #ifndef __KYK_UTILS_H
 #define __KYK_UTILS_H
 
+#include "kyk_defs.h"
+
 void kyk_print_hex(const char *label, const uint8_t *v, size_t len);
 uint8_t kyk_hex2byte(const char ch);
 void kyk_parse_hex(uint8_t *v, const char *str);
